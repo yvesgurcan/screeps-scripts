c9ea40c92a96449ba663c73578f8cbb19db3a0b3
Yves Gurcan <gurcan.yves+github@gmail.com>
2020-11-26 11:04:26 -0800

    End of game2

--- game2/role.upgrader.js
+++ game2/role.upgrader.js
@@ -1,9 +1,9 @@
 var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function (creep) {
-        if (creep.store[RESOURCE_ENERGY] == 0) {
+        if (creep.store[RESOURCE_ENERGY] === 0) {
             var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         } else {
