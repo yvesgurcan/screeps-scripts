377c274d2f8682dfc33c0cef6f8453653cab64a0
Yves Gurcan <gurcan.yves+github@gmail.com>
2020-11-26 09:24:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function getNextCreeperNumberFromType(creeperType) {
-    return Object.keys(Game.creeps).filter(creepName => creepName.includes(creeperType)).length + 1;
+function getCreepNamesFromType(creepType) {
+    return Object.keys(Game.creeps).filter(creepName => creepName.includes(creepType));
 }
 
-function getCreeperActionsFromType(creeperType) {
-    switch(creeperType) {
+function getCreepActionsFromType(creepType) {
+    switch(creepType) {
         default: {
             return [WORK, CARRY, MOVE];
         }
@@ -11,11 +11,11 @@ function getCreeperActionsFromType(creeperType) {
 }
 
 
-function spawn(creeperType) {
+function spawn(creepType) {
     function from(spawnerName) {
-        const creeperActions = getCreeperActionsFromType(creeperType);
-        const creeperNumber = getNextCreeperNumberFromType(creeperType);
-        Game.spawns[spawnerName].spawnCreep(creeperActions, `${creeperType}${creeperNumber}`);
+        const creepActions = getCreepActionsFromType(creepType);
+        const creepNumber = getCreepNamesFromType(creepType).length + 1;
+        Game.spawns[spawnerName].spawnCreep(creepActions, `${creepType}${creepNumber}`);
     }
   
     return {
@@ -23,22 +23,27 @@ function spawn(creeperType) {
     };
 }
 
+function harvest(spawnerName) {
+    return function (creepName) {
+        const creep = Game.creeps[creepName];
+        if (creep.store.getFreeCapacity() > 0) {
+            const sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+
+            return;
+        }
+
+        if (creep.transfer(Game.spawns[spawnerName], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(Game.spawns[spawnerName]);
+        }
+    }
+}
+
 module.exports.loop = function () {
     spawn('Harvester').from('Spawn1');
     
-    var creep = Game.creeps['Harvester1'];
-
-    if (creep.store.getFreeCapacity() > 0) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[0]);
-        }
-        return;
-    }
-    
-    if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE ) {
-        creep.moveTo(Game.spawns['Spawn1']);
-        return;
-    }
-    
+    const harvesterNames = getCreepNamesFromType('Harvester');
+    harvesterNames.forEach(harvest('Spawn1'));
 }
