d80394db1a231739cf8232077cd63e849c966b9c
Yves Gurcan <gurcan.yves+github@gmail.com>
2020-11-26 09:34:54 -0800

    main loop

---
+++ game1/main.js
@@ -0,0 +1,10 @@
+const { getCreepNamesFromType } = require('util');
+const harvest = require('harvest');
+const spawn = require('spawn');
+
+module.exports.loop = function () {
+    spawn('Harvester').from('Spawn1');
+
+    const harvesterNames = getCreepNamesFromType('Harvester');
+    harvesterNames.forEach(harvest('Spawn1'));
+};
